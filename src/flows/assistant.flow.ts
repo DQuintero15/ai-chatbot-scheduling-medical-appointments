5e3f677ca14dfef764ca7782666ee101f6e30c57
Daniel Quintero <quinterod164@gmail.com>
2024-11-12 07:29:14 -0500

    feat: enhance assistant flow to integrate PatientsService and manage data terms acceptance

--- src/flows/assistant.flow.ts
+++ src/flows/assistant.flow.ts
@@ -8,33 +8,48 @@ import { ASSISTANT_ID } from "src/config";
 import { provider } from "src/provider";
 import { dataTermsFlow } from "./data-terms.flow";
 import { STATE } from "src/constants/state";
+import { PatientsService } from "src/services/patients";
 
 const queueConfig: QueueConfig = { gapMilliseconds: 3000 };
 const enqueueMessage = createMessageQueue(queueConfig);
 
 const assistantFlow = addKeyword(EVENTS.ACTION)
-  .addAction(async (ctx, { state, flowDynamic, gotoFlow, endFlow }) => {
-    const dataTermsAccepted = state.get<Boolean | undefined>(
-      STATE.userAlreadyAcceptDataTerms
-    );
+  .addAction(
+    async (ctx, { state, flowDynamic, gotoFlow, endFlow, extensions }) => {
+      const patientService = extensions.patientService as PatientsService;
 
-    console.log(`[DEBUG]: dataTermsAccepted`, dataTermsAccepted);
+      const currentPhone = ctx.from.replace("57", "");
 
-    if (dataTermsAccepted === undefined) return gotoFlow(dataTermsFlow);
+      const patientPolicyStatus = await patientService.isPatientDataPolicyConfirmed(currentPhone);
 
-    if (dataTermsAccepted === false) {
-      await typing(ctx, provider);
+      if (patientPolicyStatus) {
+        await state.update({
+          [STATE.userAlreadyAcceptDataTerms]: patientPolicyStatus,
+        });
+      }
 
-      await flowDynamic(
-        [
-          "Lo siento, no podemos continuar si no acepta los términos de tratamiento de datos.",
-        ],
-        { delay: generateTimer(150, 250) }
+      const dataTermsAccepted = state.get<Boolean | undefined>(
+        STATE.userAlreadyAcceptDataTerms
       );
 
-      return endFlow();
+      console.log(`[DEBUG]: dataTermsAccepted`, dataTermsAccepted);
+
+      if (dataTermsAccepted === undefined) return gotoFlow(dataTermsFlow);
+
+      if (dataTermsAccepted === false) {
+        await typing(ctx, provider);
+
+        await flowDynamic(
+          [
+            "Lo siento, no podemos continuar si no acepta los términos de tratamiento de datos.",
+          ],
+          { delay: generateTimer(150, 250) }
+        );
+
+        return endFlow();
+      }
     }
-  })
+  )
   .addAction(async (ctx, { state, flowDynamic }) => {
     try {
       enqueueMessage(ctx, async (body) => {
@@ -59,4 +74,4 @@ const assistantFlow = addKeyword(EVENTS.ACTION)
     }
   });
 
-export { assistantFlow  };
+export { assistantFlow };
