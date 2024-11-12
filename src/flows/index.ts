eed7338c373d8c1fe0f04222566b3bc6860d3a36
Daniel Quintero <quinterod164@gmail.com>
2024-11-12 07:29:18 -0500

    feat: add idleFlow to the main flow integration

--- src/flows/index.ts
+++ src/flows/index.ts
@@ -4,11 +4,13 @@ import { welcomeFlow } from "./welcome.flow";
 import { assistantFlow } from "./assistant.flow";
 import { dataTermsFlow } from "./data-terms.flow";
 import { scheduleFlow } from "./schedule.flow";
+import { idleFlow } from "src/utils/idle";
 
 
 export default createFlow([
     welcomeFlow,
     assistantFlow,
     dataTermsFlow,
-    scheduleFlow
+    scheduleFlow,
+    idleFlow
 ])
